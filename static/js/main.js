54858a347685207a0c2dc8188c4e4591ee722017
SwaroopNandy2109 <swaroopnandy2000@gmail.com>
2021-03-20 17:49:30 +0530

    Templates Designed

--- static/js/main.js
+++ static/js/main.js
@@ -5,11 +5,17 @@ var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 var rootElement = document.documentElement;
 
 function scrollToTop() {
-    
+
     rootElement.scrollTo({
         top: 0,
         behavior: "smooth"
-      })
-  }
-  
-  scrollToTopBtn.addEventListener("click", scrollToTop)
+    })
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop)
+
+document.querySelector('#predict-btn').addEventListener('click', (e) => {
+    e.preventDefault()
+    var form = document.querySelector("#crop-predict")
+
+})
